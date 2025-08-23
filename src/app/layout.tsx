71f7b18f1bebdb75aa512b22013102007b8dfd50
Luan Santos <lvluansantos@gmail.com>
2025-08-23 11:28:59 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
-import { Geist } from "next/font/google";
 import "./globals.css";
+
+import { Metadata } from "next";
+import { Geist } from "next/font/google";
 
 const poppins = Geist({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "FSw Donalds",
+  description:
+    "Projeto de um site de fast food chamado FSw Donalds, inspirado no McDonald's, desenvolvido por um estudante de Sistemas para Internet.",
+};
 
 export default function RootLayout({
   children,
