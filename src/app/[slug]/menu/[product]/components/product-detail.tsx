c439acb6d96bd7f304f333565b69df80c98dc0d4
Luan Santos <lvluansantos@gmail.com>
2025-08-24 10:39:04 -0300

    feat: add product details page

---
+++ src/app/[slug]/menu/[product]/components/product-detail.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import { Prisma } from "@prisma/client";
+import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import Image from "next/image";
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/format-currency";
+
+interface ProductDetailsProps {
+  product: Prisma.ProductGetPayload<{
+    include: {
+      Restaurant: {
+        select: { name: true; avatarImageUrl: true; slug: true };
+      };
+    };
+  }>;
+}
+
+const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const { Restaurant } = product;
+
+  const [quantity, setQuantity] = useState<number>(1);
+
+  const handleDecreaseQuantity = () => {
+    setQuantity((prev) => {
+      if (prev === 1) return 1;
+
+      return prev - 1;
+    });
+  };
+
+  const handleIncreaseQuantity = () => {
+    setQuantity((prev) => prev + 1);
+  };
+
+  return (
+    <div className="relative z-50 mt-[1.5rem] flex flex-auto flex-col rounded-3xl p-5 pt-5">
+      <div className="flex-auto">
+        {/* Restaurant */}
+        <div className="flex items-center gap-1.5">
+          <Image
+            src={Restaurant.avatarImageUrl}
+            alt={Restaurant.name}
+            width={16}
+            height={16}
+            className="rounded-full"
+          />
+
+          <p className="text-sm text-muted-foreground">{Restaurant.name}</p>
+        </div>
+
+        {/* Nome do Produto */}
+        <h2 className="mt-1 text-xl font-semibold">{product.name}</h2>
+
+        {/* Preço e Quantidade */}
+        <div className="flex items-center justify-between">
+          <h3 className="text-xl font-semibold">
+            {formatCurrency(product.price)}
+          </h3>
+
+          <div className="flex items-center gap-3 text-center">
+            <Button
+              className="h-8 w-8 rounded-xl"
+              variant={"outline"}
+              onClick={handleDecreaseQuantity}
+            >
+              <ChevronLeftIcon />
+            </Button>
+            <p className="w-4">{quantity}</p>
+            <Button
+              className="h-8 w-8 rounded-xl"
+              variant={"destructive"}
+              onClick={handleIncreaseQuantity}
+            >
+              <ChevronRightIcon />
+            </Button>
+          </div>
+        </div>
+
+        {/* Sobre */}
+        <div className="mt-6 space-y-3">
+          <h4 className="font-semibold">Sobre</h4>
+          <p className="text-sm text-muted-foreground">{product.description}</p>
+        </div>
+
+        {/* Ingredientes */}
+        <div className="mt-6 space-y-3">
+          <div className="flex items-center gap-1">
+            <ChefHatIcon />
+            <h4 className="font-semibold">Ingredientes</h4>
+          </div>
+          <p className="text-sm text-muted-foreground">{product.description}</p>
+        </div>
+      </div>
+
+      <Button className="mt-6 w-full rounded-full">Adicionar à sacola</Button>
+    </div>
+  );
+};
+
+export default ProductDetails;
--- src/app/[slug]/menu/[product]/page.tsx
+++ src/app/[slug]/menu/[product]/page.tsx
@@ -2,18 +2,24 @@ import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
+import ProductDetails from "./components/product-detail";
 import ProductHeader from "./components/product-header";
 
 interface ProductPageProps {
-  params: Promise<{ slug: string; product: string }>;
+  params: Promise<{ product: string; slug: string }>;
 }
 
 const ProductPage = async ({ params }: ProductPageProps) => {
-  const { product } = await params;
+  const { product, slug } = await params;
 
   const productData = await db.product.findUnique({
     where: {
       id: product,
+    },
+    include: {
+      Restaurant: {
+        select: { name: true, avatarImageUrl: true, slug: true },
+      },
     },
   });
 
@@ -21,11 +27,15 @@ const ProductPage = async ({ params }: ProductPageProps) => {
     return notFound();
   }
 
+  if (productData.Restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+    return notFound();
+  }
+
   return (
-    <>
+    <div className="flex h-full flex-col">
       <ProductHeader product={productData} />
-      <div>produto</div>
-    </>
+      <ProductDetails product={productData} />
+    </div>
   );
 };
 
